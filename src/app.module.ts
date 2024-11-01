8777ffb7f8e506cb0ed52ad129a96af575958526
YungChun Tu <ken888686@gmail.com>
2024-11-01 10:21:43 +0900

    Remove app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
@@ -21,7 +19,7 @@ import { RecipesModule } from './recipes/recipes.module';
     }),
     RecipesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
